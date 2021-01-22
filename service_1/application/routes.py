af9f17ef87fbe1a7e4010c9180bafb40ba80340b
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-22 14:53:43 +0000

    Fixing sqlalchemy 4

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -8,12 +8,14 @@ class Movies(db.Model):
     weather = db.Column(db.String(50), nullable=False)
     location = db.Column(db.String(50), nullable=False)
 
+db.create_all()
+
 @app.route('/')
 @app.route('/home')
 def home():
-    location_response = requests.get('http://location_service:5000/location')
-    weather_response = requests.get('http://weather_service:5000/weather')
-    movie_response = requests.post("http://movie_service:5000/movie", data=weather_response.text)
+    location_response = requests.get('http://movie-gen_location_service:5000/location')
+    weather_response = requests.get('http://movie-gen_weather_service:5000/weather')
+    movie_response = requests.post("http://movie-gen_movie_service:5000/movie", data=weather_response.text)
 
     new_movie = Movies(name=movie_response.text,weather=weather_response.text,location=location_response.text)
     db.session.add(new_movie)
