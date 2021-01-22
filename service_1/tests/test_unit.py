6be6fa5249c712c34cafe2cbdf1bb27526744c5d
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-22 16:40:18 +0000

    Implementing mock testing 5

--- service_1/tests/test_unit.py
+++ service_1/tests/test_unit.py
@@ -1,7 +1,7 @@
 from unittest.mock import patch
 from flask import url_for
 from flask_testing import TestCase
-from application import app
+from application import app, db
 from os import getenv
 import requests_mock
 
