456154e743491d87ac75eafb54047b64de610b1a
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-23 01:50:07 +0000

    Implementing mock testing 9

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -15,3 +15,15 @@ python3 -m pytest --cov=application --cov-report xml --cov-report term-missing -
 deactivate
 cd ..
 echo "----------------------------Finished Testing Service 1----------------------------"
+
+# Test service 2
+echo "----------------------------Testing Service 2----------------------------"
+cd service_2
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+pip3 install pytest pytest-cov flask_testing requests_mock
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+deactivate
+cd ..
+echo "----------------------------Finished Testing Service 2----------------------------"
