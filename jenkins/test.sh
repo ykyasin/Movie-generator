5c43989fa1e320c7077326af399c2fcfd8cb943a
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-23 02:02:11 +0000

    Implementing mock testing 11

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y python3-venv python3-pip
 export TEST_DATABASE_URI=${TEST_DATABASE_URI}
 
 # Test service 1
-echo "----------------------------Testing Service 1----------------------------"
+echo "--------------------------------------------------------Testing Service 1-------------------------------------------------------"
 cd service_1
 python3 -m venv venv
 source venv/bin/activate
@@ -14,10 +14,10 @@ pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 cd ..
-echo "----------------------------Finished Testing Service 1----------------------------"
+echo "--------------------------------------------------------Finished Testing Service 1--------------------------------------------------------"
 
 # Test service 2
-echo "----------------------------Testing Service 2----------------------------"
+echo "--------------------------------------------------------Testing Service 2--------------------------------------------------------"
 cd service_2
 python3 -m venv venv
 source venv/bin/activate
@@ -26,4 +26,28 @@ pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 cd ..
-echo "----------------------------Finished Testing Service 2----------------------------"
+echo "--------------------------------------------------------Finished Testing Service 2--------------------------------------------------------"
+
+# Test service 3
+echo "--------------------------------------------------------Testing Service 3--------------------------------------------------------"
+cd service_3
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+pip3 install pytest pytest-cov flask_testing requests_mock
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+deactivate
+cd ..
+echo "--------------------------------------------------------Finished Testing Service 3--------------------------------------------------------"
+
+# Test service 4
+echo "--------------------------------------------------------Testing Service 4--------------------------------------------------------"
+cd service_4
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+pip3 install pytest pytest-cov flask_testing requests_mock
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+deactivate
+cd ..
+echo "--------------------------------------------------------Finished Testing Service 4--------------------------------------------------------"
