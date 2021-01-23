c3386270ef86c8065178996ef6a681d7f64f923f
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-23 21:46:18 +0000

    Found issue, checking json again 3

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -15,7 +15,7 @@ def home():
     ip_address = request.environ['HTTP_X_FORWARDED_FOR']
 
     location_response = requests.post('http://movie-gen_location_service:5000/location', data=ip_address)
-    location_response = location_response.decode('utf-8')
+    #location_response = location_response.decode('utf-8')
     #weather_response = requests.post('http://movie-gen_weather_service:5000/weather', json=location_response)
     #movie_response = requests.post("http://movie-gen_movie_service:5000/movie", data=weather_response.text)
     weather_response = "Sunny"
@@ -25,4 +25,4 @@ def home():
     #db.session.add(new_movie)
     #db.session.commit() 
 
-    return render_template('index.html', location=location_response.json(), weather=weather_response.json()["weather"][0]["main"], movie=movie_response)
+    return render_template('index.html', location=location_response.json(), weather=weather_response, movie=movie_response)
