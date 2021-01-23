2b1e52c1a8cb385242d9daccacd24aa9b2dac419
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-23 17:00:02 +0000

    Getting user ip address 2

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -2,7 +2,7 @@ from application import app
 from flask import request, Response, jsonify
 from random import choice
 
-@app.route('/location', methods=["GET"])
+@app.route('/location', methods=["POST"])
 def location():
     location = ['London','New York','Tokyo']
     ip_address = request.data.decode("utf-8")
