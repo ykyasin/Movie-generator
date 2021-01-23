773d09f67d6277fda4722393b620141afb8a9dea
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-23 21:53:01 +0000

    Found issue, checking json again 4

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -1,6 +1,7 @@
 from application import app, db 
 from flask import render_template, jsonify, request
 import requests
+import json
 
 class Movies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,6 +16,7 @@ def home():
     ip_address = request.environ['HTTP_X_FORWARDED_FOR']
 
     location_response = requests.post('http://movie-gen_location_service:5000/location', data=ip_address)
+    location_response = json.loads(js.decode("utf-8"))
     #location_response = location_response.decode('utf-8')
     #weather_response = requests.post('http://movie-gen_weather_service:5000/weather', json=location_response)
     #movie_response = requests.post("http://movie-gen_movie_service:5000/movie", data=weather_response.text)
