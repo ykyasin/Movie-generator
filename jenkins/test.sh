59bf82cfbd178ed2178de9981604a3d2c3d6f398
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-23 14:47:48 +0000

    Cleaner test.sh script

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
+# Installing dependencies 
 sudo apt-get update
 sudo apt-get install -y python3-venv python3-pip
+
+# Exporting test database uri
 export TEST_DATABASE_URI=${TEST_DATABASE_URI}
+
+# Activating virtual environment and installing requirements
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r test_requirements.txt
 
-# Test service 1
-cd service_1
-python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-cd ..
+# Testing by looping through all services
+for service in service_1 service_2 service_3 service_4; do 
+  cd $service
+  python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+  cd ..
+done
 
-# Test service 2
-cd service_2
-python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-cd ..
-
-# Test service 3
-cd service_3
-python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-cd ..
-
-# Test service 4
-cd service_4
-python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-cd ..
 deactivate
