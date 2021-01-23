c703c70d1400bed8748893dcc9b9f007eb539e79
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-23 14:37:07 +0000

    Making testing neater

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -3,47 +3,27 @@
 sudo apt-get update
 sudo apt-get install -y python3-venv python3-pip
 export TEST_DATABASE_URI=${TEST_DATABASE_URI}
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r test_requirements.txt
 
 # Test service 1
-echo "--------------------------------------------------------Testing Service 1-------------------------------------------------------"
 cd service_1
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-deactivate
 cd ..
-echo "--------------------------------------------------------Finished Testing Service 1--------------------------------------------------------"
 
 # Test service 2
-echo "--------------------------------------------------------Testing Service 2--------------------------------------------------------"
 cd service_2
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-deactivate
 cd ..
-echo "--------------------------------------------------------Finished Testing Service 2--------------------------------------------------------"
 
 # Test service 3
-echo "--------------------------------------------------------Testing Service 3--------------------------------------------------------"
 cd service_3
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-deactivate
 cd ..
-echo "--------------------------------------------------------Finished Testing Service 3--------------------------------------------------------"
 
 # Test service 4
-echo "--------------------------------------------------------Testing Service 4--------------------------------------------------------"
 cd service_4
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-deactivate
 cd ..
-echo "--------------------------------------------------------Finished Testing Service 4--------------------------------------------------------"
+deactivate
