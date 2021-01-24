503a5ecdcb926f063863f34160ba01583187b290
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-24 04:08:50 +0000

    new issue with json 19

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -2,15 +2,17 @@ from application import app
 from flask import request, Response, jsonify
 import requests
 
+@app.route('/location', methods=["GET"])
 def location():
     ip_address = request.environ['HTTP_X_FORWARDED_FOR']
     api_key = "80a3e9c5b548dfa6ca3d7cc727c1cc5e"
-    
+ 
     api_url = "http://api.ipstack.com/{}?access_key={}".format(ip_address, api_key)
 
     response = requests.get(api_url)
+    response = response.json()
 
-    return response.json()["city"]
+    return jsonify({"location":response["city"]})
 
 @app.route('/get', methods=["GET"])
 def get():
