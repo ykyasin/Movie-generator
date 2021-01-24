21b5ac692c81c76bd60ca17001f7c542c8cc3c44
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-24 14:03:03 +0000

    new issue with json 43

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -7,7 +7,7 @@ def location():
     #ip_address = request.environ['HTTP_X_FORWARDED_FOR']
     ip_address = request.data.decode("utf-8")
     api_key = "80a3e9c5b548dfa6ca3d7cc727c1cc5e"
-    api_url = "http://api.ipstack.com/{}?access_key={}".format(ip_address, api_key)
+    api_url = "http://api.ipstack.com/{}?access_key={}".format(str(ip_address), api_key)
     
 
     response = requests.get(api_url)
