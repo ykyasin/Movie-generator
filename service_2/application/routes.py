130d8c11fdf03a458415048d7734f036bcc1e933
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-24 05:11:19 +0000

    new issue with json 24

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -9,6 +9,7 @@ def location():
     api_key = "80a3e9c5b548dfa6ca3d7cc727c1cc5e"
  
     api_url = "http://api.ipstack.com/{}?access_key={}".format(ip_address, api_key)
+    
 
     response = requests.get(api_url)
     response = response.json()
