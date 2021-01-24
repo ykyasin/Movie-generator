bfe800e0325ae68a0cc82695b57ec01a03b3b608
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-24 00:58:36 +0000

    new issue with json 10

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -7,5 +7,5 @@ def location():
     api_key = "80a3e9c5b548dfa6ca3d7cc727c1cc5e"
     api_url = "http://api.ipstack.com/{}?access_key={}".format(ip_address, api_key)
     r = requests.get(api_url)
-    json_object = response.json()
-    return Response(json_object["city"], mimetype='text/plain')
+    location_response = r.json()
+    return jsonify(location_response)
