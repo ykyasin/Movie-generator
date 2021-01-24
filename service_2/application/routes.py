fc755583d95488dfc5f3e3e26455545eac695420
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-24 00:38:43 +0000

    new issue with json 7

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -6,7 +6,6 @@ def location():
     ip_address = request.data.decode("utf-8")
     api_key = "80a3e9c5b548dfa6ca3d7cc727c1cc5e"
     api_url = "http://api.ipstack.com/{}?access_key={}".format(ip_address, api_key)
-    response = requests.get(api_url)
-    r = response.json()["city"]
-    r = str(r)
-    return Response(r, mimetype='text/plain')
+    r = requests.get(api_url)
+    json_object = response.json()
+    return Response(json_object["city"], mimetype='text/plain')
