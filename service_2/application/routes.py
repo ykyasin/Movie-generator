32c861e966cb9dfed2224e20419690bb47a01526
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-24 13:20:38 +0000

    new issue with json 35

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -7,10 +7,9 @@ def location():
     #ip_address = request.environ['HTTP_X_FORWARDED_FOR']
     ip_address = "80.43.77.181"
     api_key = "80a3e9c5b548dfa6ca3d7cc727c1cc5e"
- 
     api_url = "http://api.ipstack.com/{}?access_key={}".format(ip_address, api_key)
     
 
     response = requests.get(api_url)
     response = response.json()
-    return jsonify({"location":response["city"]})
+    return Response(response, status=200, mimetype='application/json')
