b8b440116f433f591ca1e5bd86b24811b57ec973
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-24 14:44:15 +0000

    new issue with json 49

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -19,6 +19,13 @@ def home():
     weather_response = requests.post('http://movie-gen_location_service:5000/location', json=location_response.json())
     movie_response = "Avengers"
 
+    lat = location_response["latitude"]
+    lon = location_response["longitude"]
+    #city = "London"
+    api_key = "5a769573da91b09400e1d86ec1ca27bf"
+    api_url = "http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}".format(lat, lon, api_key)
+    response = requests.get(api_url)
+    movie_response = response
     #new_movie = Movies(name=movie_response.text,weather=weather_response.text,location=location_response.text)
     #db.session.add(new_movie)
     #db.session.commit() 
