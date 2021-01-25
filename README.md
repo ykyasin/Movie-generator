0522a4925c7bf24ddfebec1ba12971ed1327f389
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-25 02:28:17 +0000

    Fixing api keys 5

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# movie-generator
+#   **Movie Generator**
+
+## Contents
+* [Introduction ](#introduction )
+   * [Objective](#objective)
+   * [My Project Proposol ](#my-project-proposol )
+* [Software Architecture ](#software-architecture )
+   * [Project Tracking ](#project-tracking)
+   * [Entity Relationship Diagram](#entity-relationship-diagram)
+   * [Continuous Integration Pipeline](#continuous-integration-pipeline)
+   * [Risk Assessment](#risk-assessment)
+* [CRUD Functionality](#crud-functionality)
+   * [Login Page](#login-page)
+   * [Main Page](#main-page)
+* [CI Server](#ci-server)
+* [Testing](#testing)
+   * [Unit Testing](#unit-testing)
+   * [Integration Testing](#integration-testing)
+   * [Test Results](#test-results)
+* [Future Improvements](#future-improvements)
+* [Author](#author)
+
+
+## Introduction 
+### Objective 
+  
+### My Project Proposol  
+
+## Software Architecture 
+
+### Project Tracking 
+
+### Entity Relationship Diagram
+
+### Continuous Integration Pipeline
+
+### Risk Assessment
+
+## CRUD Functionality 
+
+### Login Page
+
+### Main Page
+
+## CI Server
+
+## Testing
+
+### Mock Testing
+
+### Test Results
+
+## Future Improvements
+
+## Author
+Yusuf Yasin
--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -20,17 +20,20 @@ def home():
     api_location = api_key("location")
     api_weather = api_key("weather")
     #location_response = requests.post('http://movie-gen_location_service:5000/location', data=ip_address) 
-    location_response = requests.post('http://movie-gen_location_service:5000/location', json={"ip_address":ip_address, "api_key":api_location}) 
-    weather_response = requests.post('http://movie-gen_weather_service:5000/weather', json=location_response.json())
+    #location_response = requests.post('http://movie-gen_location_service:5000/location', json={"ip_address":ip_address, "api_key":api_location}) 
+    #weather_response = requests.post('http://movie-gen_weather_service:5000/weather', json=location_response.json())
     
-    location = location_response.json()["city"]
-    country = location_response.json()["country_name"]
-    weather = weather_response.json()["weather"][0]["main"]
+    #location = location_response.json()["city"]
+    location = "London"
+    #country = location_response.json()["country_name"]
+    county = "United Kingdom"
+    #weather = weather_response.json()["weather"][0]["main"]
+    weather = "Rain"
     json_dict = {"country":country, "weather":weather}
     movie_response = requests.post('http://movie-gen_movie_service:5000/movie', json=json_dict)
 
-    new_movie_history = Movies(name=movie_response.text,weather=weather,location=location)
-    db.session.add(new_movie_history)
-    db.session.commit() 
+    #new_movie_history = Movies(name=movie_response.text,weather=weather,location=location)
+    #db.session.add(new_movie_history)
+    #db.session.commit() 
 
-    return render_template('index.html', location=location, weather=weather, movie=movie_response.text, ip_address =ip_address)
+    return render_template('index.html', location=location, weather=weather, movie=movie_response.text, ip_address =api_location)
