6662486792376b7feb297d17cbbeaff7f52e233b
Yusuf.Yasin <ykyasin1996@gmail.com>
2021-01-25 08:07:04 +0000

    Updated index for rolling 2

--- service_3/application/routes.py
+++ service_3/application/routes.py
@@ -4,5 +4,5 @@ from random import choice
 
 @app.route('/weather', methods=["GET"])
 def weather():
-    weather = ['Sunny','Rainy','Windy']
+    weather = ['Sunny 24','Rainy 24','Windy 24']
     return Response(choice(weather), mimetype='text/plain')
